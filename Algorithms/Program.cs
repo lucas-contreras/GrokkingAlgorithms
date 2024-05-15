1f76ab2c70e0a48c617828516d5a477f20d038b6
Lucas Contreras <lcontreras@wwnorton.com>
2024-05-15 17:19:11 -0300

    added quicksort algo

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Algorithms;
+using BenchmarkDotNet.Running;
+
+List<int> elems = new List<int>
+{
+    4,1,5,3,6,12,56,54545,13,6776,2334,23,3,
+};
+
+
+var a = QuickSort.Exec<int>(elems);
+
+//var summary = BenchmarkRunner.Run<QuickSort>();
+
+
+a.ForEach(x => Console.WriteLine(x));
---
+++ Algorithms/QuickSort.cs
@@ -0,0 +1,46 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Collections;
+
+namespace Algorithms
+{
+    [MemoryDiagnoser]
+    public class QuickSort
+    {
+        [Benchmark]
+        public static List<int> Exec<T>(List<int> array) where T : struct
+        {
+            if (array.Count < 2)
+            {
+                return array;
+            }
+
+            int pivot = array[0];
+            List<int> less = new List<int>();
+            List<int> greater = new List<int>();
+
+            for (int x = 1; x < array.Count; x++)
+            {
+                var value = array[x];
+                if (value <= pivot)
+                {
+                    less.Add(value);
+                }
+                else
+                {
+                    greater.Add(value);
+                }
+            }
+
+            //List<int> less = array.FindAll(x => x <= pivot);
+            //List<int> greater = array.FindAll(x => x > pivot);
+
+            var result1 = Exec<int>(less);
+            var result2 = Exec<int>(greater);
+
+            result1.Add(pivot);
+            result1.AddRange(result2);
+
+            return result1;
+        }
+    }
+}
